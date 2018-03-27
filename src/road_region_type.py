81d42d7648c943145dc5344b339a0d2f2971984d
Wouter Leeftink <wouter.leeftink@mediaan.com>
2018-03-27 15:52:42 +0200

    Finished multivariate regression on traffic volume vs number of accidents

--- src/road_region_type.py
+++ src/road_region_type.py
@@ -8,7 +8,8 @@ Created on Mon Mar 26 20:28:34 2018
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 fiveseven = pd.read_csv('accidents_2005_to_2007.csv')
 nineeleven = pd.read_csv('accidents_2009_to_2011.csv')
@@ -38,8 +39,10 @@ broad_accidents = accidents[accidents['1st_Road_Class'] == 4]
 broad = broad_accidents.groupby('1st_Road_Number')['Accident_Index'].nunique().sort_values(ascending = False)
 broad = broad.rename(lambda x: 'B' + str(x))
 
-all_roads = [motorway, aroad, broad]
-all_roads = pd.concat(all_roads).sort_values(ascending = False)
+all_roads0 = [motorway, aroad, broad]
+all_roads0 = pd.concat(all_roads0)
+all_roads = all_roads0
+all_roads = all_roads.sort_values(ascending = False)
 
 top_10_motorway = motorway.head(10)
 top_10_motorway.plot('bar')
@@ -53,12 +56,11 @@ top_10_broad.plot('bar')
 top_10_allroad = all_roads.head(10)
 top_10_allroad.plot('bar')
 
-print(all_roads.index)
 columns = ['Road', 'PedalCycles', 'Motorcycles', 'CarsTaxis', 'BusesCoaches', 'LightGoodsVehicles', 'AllHGVs', 'AllMotorVehicles', 'Accidents']
 roadframe = pd.DataFrame(columns = columns)
-for road in all_roads.index:
+for road in all_roads0.index:
     road_matrix = aadf[aadf['Road'] == road]
-    if not road_matrix.empty and not np.isnan(all_roads[road]):
+    if not road_matrix.empty and not np.isnan(all_roads0[road]):
         pedals = road_matrix['PedalCycles'].mean()
         motor = road_matrix['Motorcycles'].mean()
         cars = road_matrix['CarsTaxis'].mean()
@@ -74,5 +76,18 @@ for road in all_roads.index:
                                       'LightGoodsVehicles': lightgoods,
                                       'AllHGVs': heavygoods,
                                       'AllMotorVehicles': allmotorvehicles,
-                                      'Accidents': float(all_roads[road])}, ignore_index = True)
+                                      'Accidents': float(all_roads0[road])}, ignore_index = True)
 correlation = roadframe.corr()    
+frame_X = roadframe[['PedalCycles', 'Motorcycles', 'CarsTaxis', 'BusesCoaches', 'LightGoodsVehicles', 'AllHGVs']]
+frame_Y = roadframe['Accidents']
+msk = np.random.rand(len(frame_X)) < 0.8
+train_X = frame_X[msk]
+test_X = frame_X[~msk]
+train_Y = frame_Y[msk]
+test_Y = frame_Y[~msk]
+
+model = linear_model.LinearRegression()
+model_fit = model.fit(train_X, train_Y)
+pred_Y = model_fit.predict(test_X)
+print(mean_squared_error(test_Y, pred_Y))
+print(r2_score(test_Y, pred_Y))
